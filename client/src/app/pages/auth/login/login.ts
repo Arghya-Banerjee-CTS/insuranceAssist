0db44c869197f68d5707cb1e04b4f10af96fe65a
Philip Bulla <2438073@cognizant.com>
2025-09-17 14:11:03 +0530

    Role-based login implemented

--- client/src/app/pages/auth/login/login.ts
+++ client/src/app/pages/auth/login/login.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { environment } from '../../../../environments/environment.development';
@@ -13,25 +13,48 @@ import { LoginService } from '../../../core/services/login';
   styleUrl: './login.css'
 })
 export class Login {
+
+  router = inject(Router);
+  loginService = inject(LoginService);
+
   loginForm: FormGroup = new FormGroup({
+    role: new FormControl(""),
     username : new FormControl(""),
     password : new FormControl("")
   });
 
-  router = inject(Router);
-  loginService = inject(LoginService);
-  private http = inject(HttpClient);
 
   onLogin() {
     const formValue = this.loginForm.value;
-    // console.log(formValue);
+    const selectedRole = this.loginForm.get('role')?.value;
 
     this.loginService.onLogin(formValue).subscribe({
       next: (response:any) => {
         
         if(response && response.token){
           localStorage.setItem('token', response.token);
-          this.router.navigateByUrl("/dashboard/client");
+          localStorage.setItem('role', response.role);
+          localStorage.setItem('userId', response.userId);
+
+          console.log("Role from server: ", response.role);
+          console.log("Selected Role: ", selectedRole);
+          // debugger;
+
+          if(response.role === 'CLIENT' && 
+            this.loginForm.value.role === 'client'
+          ){
+            // debugger;
+            this.router.navigateByUrl("/dashboard/client");
+          }
+          else if(response.role === 'AGENT' && 
+            this.loginForm.value.role === 'agent'
+          ){
+            // debugger;
+          this.router.navigateByUrl("/dashboard/agent");
+          }
+          else if(response.role !== selectedRole.toUpperCase()){
+            alert(`You are registered as ${response.role}. Please select the correct role.`);
+          }
         }
         else{
           alert("Invalid response from server.");
