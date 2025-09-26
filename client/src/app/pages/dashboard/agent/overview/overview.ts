d759ea2b7cc31341e160ba694782142d7acfc1af
Arghya-Banerjee-CTS <Arghya.Banerjee@cognizant.com>
2025-09-26 15:06:57 +0530

    IN REVIEW badge text color dark to light

--- client/src/app/pages/dashboard/agent/overview/overview.ts
+++ client/src/app/pages/dashboard/agent/overview/overview.ts
@@ -4,7 +4,7 @@ import { ClaimManagement } from '../../../../core/services/api/AgentDashBoard/cl
 import { CommonModule, DatePipe } from '@angular/common';
 import { UrlSegment } from '@angular/router';
 
-type Agent =  {
+type Agent = {
   username: string;
   name: string;
   gender: string;
@@ -15,77 +15,69 @@ type Agent =  {
 }
 
 type Claim = {
-  claimId: string;           
-  policyId: string;          
-  openDate: string;          
+  claimId: string;
+  policyId: string;
+  openDate: string;
   procedureNotes: string;
   claimType: string;
   status: string;
-  claimAmount: number; 
-  updatedAt:string;      
+  claimAmount: number;
+  updatedAt: string;
 };
 
 
 
 @Component({
   selector: 'app-overview',
-  imports: [DatePipe,CommonModule],
+  imports: [DatePipe, CommonModule],
   templateUrl: './overview.html',
   styleUrl: './overview.css'
 })
-export class Overview implements OnInit{
-  
-  private profileService:ClaimManagement = inject(ClaimManagement);
-  private userId!:string|null;
-  agentDetails!:Agent;
-  totalClients!:Number;
-  Claims!:Claim[];
-  activePoliciesNo!:Number;
-  activeClients!:Number;
-  totalAmountClaimPassed!:Number;
-  latestClaims!:Claim[]
+export class Overview implements OnInit {
+
+  private profileService: ClaimManagement = inject(ClaimManagement);
+  private userId!: string | null;
+  agentDetails!: Agent;
+  totalClients!: Number;
+  Claims!: Claim[];
+  activePoliciesNo!: Number;
+  activeClients!: Number;
+  totalAmountClaimPassed!: Number;
+  latestClaims!: Claim[]
 
   ngOnInit(): void {
-      this.userId = localStorage.getItem('userId');
-      this.profileService.getAgentDetails(this.userId).subscribe({
-        next: (element) => {
-          this.agentDetails = element;
-          console.log(this.agentDetails)
-        },
-        error:(element) => {
-          console.log("error occurred");
-        }
+    this.userId = localStorage.getItem('userId');
+    this.profileService.getAgentDetails(this.userId).subscribe({
+      next: (element) => {
+        this.agentDetails = element;
+        console.log(this.agentDetails)
+      },
+      error: (element) => {
+        console.log("error occurred");
       }
-      );
+    }
+    );
 
-      this.profileService.getClaimsByAgentId(this.userId).subscribe({
-        next: (element) => {
-          this.Claims = element;
-          this.totalClients = this.Claims.length;
-          // console.log(this.totalClients);
-          const activeArrayList = this.Claims.filter((el) => {
-            el.status === 'REVIEW' || el.status === 'CREATED'
-          });
-          this.activeClients = activeArrayList.length;
-          console.log(this.activeClients)
-          this.totalAmountClaimPassed = this.Claims
-            .filter((el: Claim) => el.status === 'APPROVED') 
-            .reduce((sum, el) => sum + el.claimAmount,0);  
-          this.latestClaims = this.Claims
-          .sort((a: Claim, b: Claim) => 
+    this.profileService.getClaimsByAgentId(this.userId).subscribe({
+      next: (element) => {
+        this.Claims = element;
+        this.totalClients = this.Claims.length;
+        // console.log(this.totalClients);
+        const activeArrayList = this.Claims.filter((el) => {
+          el.status === 'IN REVIEW' || el.status === 'CREATED'
+        });
+        this.activeClients = activeArrayList.length;
+        console.log(this.activeClients)
+        this.totalAmountClaimPassed = this.Claims
+          .filter((el: Claim) => el.status === 'APPROVED')
+          .reduce((sum, el) => sum + el.claimAmount, 0);
+        this.latestClaims = this.Claims
+          .sort((a: Claim, b: Claim) =>
             new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime()
           )
           .slice(0, 3);
-        }
-      })
+      }
+    })
+  }
 
-
-
-      
-
-      
-
-      
-  } 
-     
 }
