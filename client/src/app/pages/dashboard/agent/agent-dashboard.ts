ff597d319e926119c67b8fadbbd8d4c76d56b2d7
Philip Bulla <2438073@cognizant.com>
2025-09-30 22:54:15 +0530

    Agent network hospitals page UI and Implementation  added

--- client/src/app/pages/dashboard/agent/agent-dashboard.ts
+++ client/src/app/pages/dashboard/agent/agent-dashboard.ts
@@ -1,17 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Overview } from './overview/overview';
 import { ClaimApproval } from "./claim-approval/claim-approval";
 import { ClaimHistory } from './claim-history/claim-history';
+import { HttpClient } from '@angular/common/http';
+import { HospitalService } from '../../../core/services/api/hospital/hospital-service';
+import { AgentNetworkHospitals } from './agent-network-hospitals/agent-network-hospitals';
 
 @Component({
   selector: 'app-agent',
-  imports: [Overview, ClaimApproval,ClaimHistory],
+  imports: [Overview, ClaimApproval,ClaimHistory, AgentNetworkHospitals],
   templateUrl: './agent.html',
   styleUrl: './agent.css'
 })
 export class AgentDashboard {
-  activeTab: 'overview' | 'claim-approval' | 'claim-history' | 'claim-status-log' = 'overview';
-  setTab(tab: 'overview' | 'claim-approval' | 'claim-history' | 'claim-status-log') {
+  activeTab: 'overview' | 'claim-approval' | 'claim-history' | 'network-hospitals' = 'overview';
+  setTab(tab: 'overview' | 'claim-approval' | 'claim-history' | 'network-hospitals') {
     this.activeTab = tab;
   }
+
+  http = inject(HttpClient);
+
+  private hospitalService = inject(HospitalService);
+    
+  hospitalList: any[] = [];
+
+  
+  ngOnInit(): void{
+    this.hospitalService.onGetAllHospitals().subscribe({
+      next: (result: any) => {
+        this.hospitalList = result;
+      },
+      error: (error: any) => {
+        console.error(error.message);
+      }
+    });
+    
+    
+  }
+
+
 }
