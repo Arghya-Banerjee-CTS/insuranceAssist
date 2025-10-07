d3e7e15c83bc20d80b92f0070896cf17e77f16a4
Philip Bulla <2438073@cognizant.com>
2025-10-07 10:31:51 +0530

    claim amount update on pre authorization claim approval

--- server/src/main/java/com/example/insuranceAssist/service/ClaimService.java
+++ server/src/main/java/com/example/insuranceAssist/service/ClaimService.java
@@ -219,8 +219,6 @@ public class ClaimService {
         StatusTypeMaster approvedStatus = statusTypeMasterRepository.findById(3L)
                 .orElseThrow(() -> new StatusTypeNotFoundException("Status type not found with id: " + 3));
 
-        claim.setClaimAmount(claimAmount);
-
         authorizationRepository.save(claim);
 
         if(currStatus == approvedStatus) {
@@ -247,7 +245,7 @@ public class ClaimService {
                 claim.getProcedureNotes(),
                 claim.getClaimType().getClaimType(),
                 claim.getStatus().getStatusType(),
-                claimAmount,
+                claim.getClaimAmount(),
                 claim.getUpdatedAt()
         );
 
